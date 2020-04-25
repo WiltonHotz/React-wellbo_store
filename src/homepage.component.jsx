58555af999cd13dd82a91d2cdffab8175947e96f
WiltonHotz <v.horwing@gmail.com>
2020-04-25 16:36:54 +0200

    created project with create-react-app and added files for homepage

---
+++ src/homepage.component.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './homepage.styles.scss'
+
+const HomePage = () => (
+  <div className='homepage'>
+    <div className='directory-menu'>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>TESLA</h1>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>FOOD MEMBERS ATE</h1>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>SOUS-VIDE</h1>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>STYLISH FACE MASKS</h1>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>BTC BEARMARKET MEMES</h1>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+export default HomePage
